5fcfd3c0bfca4687c6de1d1ea5a53cbf950799c1
Yonatan Atlas <atlasyonatan@gmail.com>
2022-08-23 22:56:21 +0300

    cleanup and organize

--- solve.py
+++ solve.py
@@ -9,6 +9,13 @@ import numpy as np
 logging.basicConfig(level=logging.DEBUG)
 
 HEIGHT, WIDTH = 5, 5
+CONSTANTS = {
+    (0, 0): True,
+    (2, 2): True,
+    (0, 4): True
+}
+SOLUTION_COUNT = 10
+
 SIZE = WIDTH * HEIGHT
 coordinate, cell_number = coordinate_l(WIDTH), cell_number_l(WIDTH)
 print(f"WIDTH = {WIDTH}, HEIGHT = {HEIGHT}")
@@ -25,117 +32,111 @@ for index in np.ndindex(*grid.shape):
 # numbers = [ [Int(f'number_{r}_{c}') for c in range(HEIGHT)] for r in range(WIDTH) ]
 logging.debug(f"{time() - t:f} seconds")
 
-# logging.debug("constraining: no 2x2 shaded")
-# t = time()
-# # 2x2 shaded cells are not allowed
-# for x in range(0, WIDTH - 1):
-#     for y in range(0, HEIGHT - 1):
-#         s.add(Not(And([grid[x][y], grid[x][y + 1], grid[x + 1][y], grid[x + 1][y + 1]])))
-# logging.debug(f"{time() - t:f} seconds")
+logging.debug("constraining: no 2x2 shaded")
+t = time()
+# 2x2 shaded cells are not allowed
+for x in range(0, WIDTH - 1):
+    for y in range(0, HEIGHT - 1):
+        s.add(Not(And([grid[x][y], grid[x][y + 1], grid[x + 1][y], grid[x + 1][y + 1]])))
+logging.debug(f"{time() - t:f} seconds")
 
-# shape = (SIZE - 2, SIZE, SIZE)
-# adjacency_k = np.empty(shape, dtype=ExprRef)
-# for index in np.ndindex(*adjacency_k.shape):
-#     adjacency_k[index] = Bool(f"adjacency_{'_'.join(str(v) for v in index)}")
+shape = (SIZE - 2, SIZE, SIZE)
+adjacency_k = np.empty(shape, dtype=ExprRef)
+for index in np.ndindex(*adjacency_k.shape):
+    adjacency_k[index] = Bool(f"adjacency_{'_'.join(str(v) for v in index)}")
 
-# logging.debug("constructing: adjacency_k")
-# t0 = time()
-# # The Adjacency Matrix (tam) defined to be 1 when cells i and j in grid are shaded and connected, otherwise 0
-# for x in range(WIDTH):
-#     for y in range(HEIGHT - 1):
-#         x1, y1 = x, y
-#         x2, y2 = x, y + 1
-#         i = cell_number(x1, y1)
-#         j = cell_number(x2, y2)
-#         both_shaded = And(grid[x][y], grid[x][y + 1])
-#         adjacency_k[0][i][j] = both_shaded  # z3bool_to_int(both_shaded)
-# for x in range(WIDTH - 1):
-#     for y in range(HEIGHT):
-#         i = cell_number(x, y)
-#         j = cell_number(x + 1, y)
-#         both_shaded = And(grid[x][y], grid[x + 1][y])
-#         adjacency_k[0][i][j] = both_shaded  # z3bool_to_int(both_shaded)
-# logging.debug(f"{time() - t:f} seconds")
+logging.debug("constructing: adjacency matrix")
+t0 = time()
+# The Adjacency Matrix (tam) defined to be 1 when cells i and j in grid are shaded and connected, otherwise 0
+for x in range(WIDTH):
+    for y in range(HEIGHT - 1):
+        x1, y1 = x, y
+        x2, y2 = x, y + 1
+        i = cell_number(x1, y1)
+        j = cell_number(x2, y2)
+        both_shaded = And(grid[x][y], grid[x][y + 1])
+        adjacency_k[0][i][j] = both_shaded  # z3bool_to_int(both_shaded)
+for x in range(WIDTH - 1):
+    for y in range(HEIGHT):
+        i = cell_number(x, y)
+        j = cell_number(x + 1, y)
+        both_shaded = And(grid[x][y], grid[x + 1][y])
+        adjacency_k[0][i][j] = both_shaded  # z3bool_to_int(both_shaded)
+logging.debug(f"{time() - t:f} seconds")
 
-# logging.debug("constructing: adjacency_k")
-# t0 = time()
-# # powers of The Adjacency Matrix
-# for k in range(1, adjacency_k.shape[0]):
-#     adjacency_k[k] = adjacency_k[0] * adjacency_k[k - 1]  # dot product
-# logging.debug(f"{time() - t:f} seconds")
+logging.debug("constructing: adjacency_k")
+t0 = time()
+# powers of The Adjacency Matrix
+for k in range(1, adjacency_k.shape[0]):
+    adjacency_k[k] = adjacency_k[0] * adjacency_k[k - 1]  # dot product
+logging.debug(f"{time() - t:f} seconds")
 
-# logging.debug("constructing: sum_adjacency_k")
-# t0 = time()
-# # matrix for the sum of all adjacency^k
-# adjacency_k_sum = np.ndarray.sum(adjacency_k, axis=0)
-# logging.debug(f"{time() - t:f} seconds")
+logging.debug("constructing: sum_adjacency_k")
+t0 = time()
+# matrix for the sum of all adjacency^k
+adjacency_k_sum = np.ndarray.sum(adjacency_k, axis=0)
+logging.debug(f"{time() - t:f} seconds")
 
-# logging.debug("constraining: sum of adjacency^k is nonzero for shaded cell pairs")
-# t0 = time()
-# # constrain the sum of adjacency^k for k in [1..SIZE-1], is positive for all shaded cells
-# for i in range(SIZE):
-#     for j in range(SIZE):
-#         x1, y1 = coordinate(i)
-#         x2, y2 = coordinate(j)
-#         shaded = And(grid[x1][y1], grid[x2][y2])
-#         s.add(shaded == adjacency_k_sum[i][j])
-# logging.debug(f"{time() - t:f} seconds")
+logging.debug("constraining: sum of adjacency^k is nonzero for shaded cell pairs")
+t0 = time()
+# constrain the sum of adjacency^k for k in [1..SIZE-1], is positive for all shaded cells
+for i in range(SIZE):
+    for j in range(SIZE):
+        x1, y1 = coordinate(i)
+        x2, y2 = coordinate(j)
+        shaded = And(grid[x1][y1], grid[x2][y2])
+        s.add(shaded == adjacency_k_sum[i][j])
+logging.debug(f"{time() - t:f} seconds")
 
-# logging.debug("constraining: constants")
-# t0 = time()
-#
-# for c in constant:
-#     print(c, " will be shaded")
-#     s.add(grid[c[0]][c[1]])
-# logging.debug(f"{time() - t:f} seconds")
+logging.debug("finished constraining puzzle rules")
 
 logging.debug("checking sat")
-t0 = time()
+t = time()
 # are we SAT?
 sat_result = s.check()
-t1 = time()
-logging.debug(f"{t1 - t0:f} seconds")
+logging.debug(f"{time() - t:f} seconds")
 
-if s.check() == unsat:
+if sat_result == unsat:
     print("We are not SAT D:")
     exit(1)
 
-print("Adding constants")
+print("constraining: constants")
 # hard set these coordinates
-constant_coordinates = [(0, 0), (2, 2), (0, 4)]
-for x, y in constant_coordinates:
-    s.add(grid[x][y])
-logging.debug("checking sat")
-t0 = time()
-# are we SAT?
-sat_result = s.check()
-t1 = time()
-logging.debug(f"{t1 - t0:f} seconds")
-if s.check() == unsat:
-    print("We are not SAT D:")
-    exit(1)
+for key, value in CONSTANTS.items():
+    x, y = key
+    t = time()
+    constraint = f"'{key} == {value}'"
+    logging.debug(f"constraining: {constraint}")
+    s.add(grid[x][y] == value)
+    logging.debug(f"{time() - t:f} seconds")
 
-decls = set([d.name() for d in s.model().decls()])
-free_terms = []  # [d for d in [str(grid[index]) for index in ] if d in decls]
+    logging.debug("checking sat")
+    t = time()
+    # are we SAT?
+    sat_result = s.check()
+    logging.debug(f"{time() - t:f} seconds")
+
+    if sat_result == unsat:
+        print(f"This constraint: {constraint} causes an unsat D:")
+        exit(1)
+
+declarations = set([d.name() for d in s.model().decls()])
+free_terms = []
 for index in np.ndindex(*grid.shape):
     term = grid[index]
-    if str(term) not in decls:
+    if str(term) not in declarations:
         free_terms.append(term)
 solutions = all_smt(s, free_terms)
-for i, m in enumerate(islice(solutions, 4), start=1):
+s = islice(solutions, SOLUTION_COUNT)
+for i, m in enumerate(s, start=1):
     m: ModelRef
 
 
     def display_cell(x, y):
         r = grid[x][y]
-        # declr =
-        # arity = declr.arity()
-        # if arity == 0:
         return '#' if m.eval(r) else ' '
-        # return str(arity)
 
 
-    print(f"Sat #{i}:")
+    print(f"Solution #{i}:")
     mat_display(grid, display_cell)
     print()
-# print(m)
