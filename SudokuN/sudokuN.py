21f3acc64e3296a30ef3e0b0dd0d5d737492a832
Yonatan Atlas <atlasyonatan@gmail.com>
2022-08-28 14:09:32 +0300

    using seq

--- SudokuN/sudokuN.py
+++ SudokuN/sudokuN.py
@@ -8,20 +8,20 @@ from math import prod
 s = Solver()
 
 N = Int('N')
-s.add(N == 1)
+s.add(N == 3)
 
 box_shape = N, N
 box_length = prod(box_shape)
 
 grid_shape = tuple([d * d for d in box_shape])
 grid_length = prod(grid_shape)
-
-grid = Array('grid', IntSort(), IntSort())
+grid = Const('grid', SeqSort(IntSort()))
+s.add(Length(grid) == grid_length)
 
 grid_i = Int('grid_i')
 i_in_range = in_range(grid_i, 0, grid_length)
 
-is_digit = in_range(grid[grid_i], 1, grid_length + 1)
+is_digit = in_range(grid[grid_i], 1, box_length + 1)
 is_zero = grid[grid_i] == 0
 s.add(ForAll([grid_i], If(i_in_range, is_digit, is_zero)))
 
@@ -40,23 +40,23 @@ x2, y2 = sd_to_md(grid_j, grid_shape)
 
 # row:
 same_row = y1 == y2
-s.add(ForAll([grid_i, grid_j], Implies(
-    same_row,
-    ij_distinct)))
+# s.add(ForAll([grid_i, grid_j], Implies(
+#     same_row,
+#     ij_distinct)))
 
 # col:
-# same_col = x1 == x2
+same_col = x1 == x2
 # s.add(ForAll([grid_i, grid_j], Implies(
 #     same_col,
 #     ij_distinct)))
-#
-#
-# def grid_to_box(i):
-#     return blur_sd(i, grid_shape, box_shape)
-#
-#
-# box_i, box_j = grid_to_box(grid_i), grid_to_box(grid_j)
-# same_box = box_i == box_j
+
+
+def grid_to_box(i):
+    return blur_sd(i, grid_shape, box_shape)
+
+
+box_i, box_j = grid_to_box(grid_i), grid_to_box(grid_j)
+same_box = box_i == box_j
 # s.add(ForAll([grid_i, grid_j], Implies(
 #     same_box,
 #     ij_distinct)))
@@ -69,22 +69,12 @@ if check_sat_result == unsat:
     exit(1)
 
 m = s.model()
-grid_shape_v = np.vectorize(lambda expr: m.eval(expr, model_completion=True).as_long())(grid_shape)
-# grid_v = evaluate(grid)
-# grid_length_v = evaluate(grid_length)
-# grid_v = m.eval(grid, model_completion=True)
-# s.add(grid_v)
-index = Int('index')
-value = m.eval(grid[index])
 
+eval_int = np.vectorize(lambda expr: m.eval(expr, model_completion=True).as_long())
+grid_length_v = eval_int(grid_length)
+grid_shape_v = eval_int(grid_shape)
 board = np.empty(grid_shape_v, dtype=int)
-# for coordinates in np.ndindex(*grid_shape_v):
-#     i = md_to_sd(coordinates, grid_shape_v)
-#     solver = Solver()
-#     solver.add(index == IntVal(i))
-#     if solver.check() == unsat:
-#         raise ValueError(f"no solution for {index=}={i} and {value=}=\n{value}")
-#     model = solver.model()
-#     v = model.evaluate(value).as_long()
-#     board[coordinates] = v
+for i in range(grid_length_v):
+    coordinates = sd_to_md(i, grid_shape_v)
+    board[coordinates] = eval_int(grid[IntVal(i)])
 mat_display(board)
