87836221ac5642ecbf8d44bd486209dd41e2f662
Yonatan Atlas <atlasyonatan@gmail.com>
2023-06-29 01:39:28 +0300

    working adjecancy_pow_1

--- model.py
+++ model.py
@@ -5,14 +5,14 @@ from functools import reduce
 from mdArray import sd_to_md, md_to_sd
 
 
-compose = lambda *fs: reduce(lambda f,g: lambda *a, **kw: f(g(*a, **kw)), fs)
+compose = lambda *fs: reduce(lambda f, g: lambda *a, **kw: f(g(*a, **kw)), fs)
 
 width, height = Ints("width height")
 
-coordinate = sd_to_md((width, height))
-index = md_to_sd((width, height))
-
-board = Array("1d-board", IntSort(), BoolSort())
+board = Array("2d-board", IntSort(), BoolSort())
+board_shape = (width, height)
+coordinate = sd_to_md(board_shape)
+index = md_to_sd(board_shape)
 
 # rule: no 2x2 shaded cells
 x, y = Ints("x y")
@@ -27,24 +27,47 @@ rule_no_2x2 = ForAll(
 
 
 # rule: connectedness
-# board = Array(f"kth-view", IntSort(), IntSort())
+adjacency_pow_k = Array("3d-adjacency_pow_k", IntSort(), IntSort())
+adjacency_pow_k_shape = (
+    width * width * height * height,
+    width * height,
+    width * height,
+)
+adjacency_coordinate = sd_to_md(adjacency_pow_k_shape)
+adjacency_index = md_to_sd(adjacency_pow_k_shape)
 
+i, j = Ints("i j")
+
+abs_z3 = lambda v: If(v >= 0, v, -v)
+difference = abs_z3(i - j)
+are_cardinal_neighbors = Or(
+    difference == width,
+    And(difference == 1, abs_z3(i % width - j % height) == 1),
+)
+i_and_j_in_bounds = And(i>=0, i<width*height, j>=0, j<width*height)
+# are_shaded = And(board[i], board[j])
+adjacency_pow_1 = ForAll(
+    [i, j],
+    Implies(i_and_j_in_bounds, adjacency_pow_k[adjacency_index(1, i, j)] == If(are_cardinal_neighbors, 1, 0))
+)
 
 ##########################
 if __name__ == "__main__":
     WIDTH, HEIGHT = 3, 3
 
-    print(rule_no_2x2.sexpr())
-    index = md_to_sd((WIDTH, HEIGHT))
+    # board_index = md_to_sd((WIDTH, HEIGHT))
     solver = Solver()
 
     solver.add(width == WIDTH)
     solver.add(height == HEIGHT)
-    solver.add(rule_no_2x2)
+    # solver.add(rule_no_2x2)
 
-    solver.add(board[index(0,0)] == True)
-    solver.add(board[index(0,1)] == True)
-    solver.add(board[index(1,0)] == True)
+    solver.add(adjacency_pow_1)
+
+    print(solver.sexpr())
+    # solver.add(board[index(0, 0)] == True)
+    # solver.add(board[index(0, 1)] == True)
+    # solver.add(board[index(1, 0)] == True)
     # solver.add(board[index(1,1)] == True)
 
     if solver.check() == unsat:
@@ -52,14 +75,33 @@ if __name__ == "__main__":
         exit(1)
 
     model = solver.model()
+
+    comp_eval = lambda e: model.eval(e, model_completion=True)
+
+    board_shape = tuple(map(comp_eval, board_shape))
+
+    # adjacency_shape = tuple(map(comp_eval, adjacency_pow_k_shape[1:]))
+
     from tools import bool_display, mat_display
     import numpy as np
 
-    eval_bool_mat = np.vectorize(
-        compose(is_true, lambda e: model.eval(e, model_completion=True))
-    )
-    bool_to_char = np.vectorize(bool_display)
+    eval_bool_mat = np.vectorize(compose(is_true, comp_eval))
 
-    board_elements = [[board[index(x,y)] for x in range(WIDTH)] for y in range(HEIGHT)]
+    eval_int_mat = np.vectorize(lambda e: comp_eval(e).as_long())
+
+    board_elements = [[board[index(x, y)] for x in range(WIDTH)] for y in range(HEIGHT)]
+
+    adjacency_matrix = [
+        [
+            adjacency_pow_k[adjacency_index(1, x, y)]
+            for x in range(comp_eval(adjacency_pow_k_shape[1]).as_long())
+        ]
+        for y in range(comp_eval(adjacency_pow_k_shape[2]).as_long())
+    ]
+
     shading = eval_bool_mat(board_elements)
-    mat_display(bool_to_char(shading))
+    adjacency = eval_int_mat(adjacency_matrix)
+
+    # bool_to_char = np.vectorize(bool_display)
+    # mat_display(bool_to_char(shading))
+    mat_display(adjacency)
